829e3bbfe53abff7e232736423c6a6adac4f559a
virusss404 <152058442+virusss404@users.noreply.github.com>
2024-09-21 23:05:22 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+print("""
+\x1b[38;2;255;20;147m╔═╗╦╔═╗╔╦╗  ╔═╗╦ ╦╔╦╗╔═╗╔╦╗
+\x1b[38;2;255;20;147m╚═╗║╠═╣║║║  ╠═╣╠═╣║║║║╣  ║║
+\x1b[38;2;255;20;147m╚═╝╩╩ ╩╩ ╩  ╩ ╩╩ ╩╩ ╩╚═╝═╩╝\x1b[38;2;0;255;58m>(setup)
+""") 
+
+os.system("pip install keyboard")
+os.system("pip install PyAutoGUI")
+os.system("pip install cloudmesh-sys")
+os.system("pip install colorama")
+os.system("pip install PyGetWindow")
+os.system("pip install funcnodes-opencv")
+os.system("pip install pynput")
+os.system("pip install mouse")
+
+
+
+print("Done.")
